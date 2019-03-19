019af9befed66a1b11278c8610ac9d10515812d2
zeroone001 <ilylys@163.com>
2019-03-19 14:11:43 +0800

    refs #0000 浏览器跨域的解决方案

--- cross-domain.js
+++ cross-domain.js
@@ -5,3 +5,7 @@
     2， 跨域
     3， XHR （用jsonp（<script>）,或者<img>都可以解决）
 */
+/*
+    浏览器限制的解决方案： 控制台：  open -n /Applications/Google\ Chrome.app/ —args —disable-web-security  —user-data-dir=/Users/smzdm/MyChromeDevUserData/
+
+*/
