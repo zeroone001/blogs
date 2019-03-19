f51c7f9809493fabbb79689efc4a97a615cb269a
zeroone001 <ilylys@163.com>
2019-03-19 10:31:16 +0800

    refs #0000 last-child 的问题

---
+++ last-child.js
@@ -0,0 +1,15 @@
+/*
+    关于伪类的一些记录
+*/
+/*<ul>
+  <li>此元素背景色不是lime</li>
+  <li>我的也不是lime。</li>
+  <li>我的才是lime！ :)</li>
+  <div>dasdas</div>
+</ul>*/
+/*li:last-child {
+  background-color: lime;
+}*/
+/*
+    因为最后多加了一个div导致last-child不起作用，解决方案：nth-last-child(2) 代表倒数第二个元素
+*/
