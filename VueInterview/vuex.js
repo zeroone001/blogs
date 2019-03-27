0d687bcc563f19482b9117c696fe5bfce752b42c
zeroone001 <ilylys@163.com>
2019-03-27 21:50:48 +0800

    vue

--- VueInterview/vuex.js
+++ VueInterview/vuex.js
@@ -6,3 +6,14 @@
     modules： 将state分成多个modules，便于管理
     
 */
+/*
+    state特性： 
+        * 通过mapState把全局的state和getters影射到当前组件的computed中
+        * 
+    getter:
+        * 对state进行计算，是store的计算属性
+    mutations：
+        * Action提交的是mutation，不是直接变更状态
+        * 可以包含异步操作
+    
+*/
