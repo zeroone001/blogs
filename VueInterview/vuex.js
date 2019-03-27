c2e6dd5dfe9da2c8447bebdf88478542d34c31a7
zeroone001 <ilylys@163.com>
2019-03-27 22:18:07 +0800

    vuex actions

--- VueInterview/vuex.js
+++ VueInterview/vuex.js
@@ -1,4 +1,7 @@
 /*
+    https://vuex.vuejs.org/zh/guide/actions.html
+*/
+/*
     state: 状态中心
     mutations: 更改状态；
     actions： 异步更改状态
@@ -15,5 +18,42 @@
     mutations：
         * Action提交的是mutation，不是直接变更状态
         * 可以包含异步操作
+    mutation 必须是同步函数，因为在回掉函数中进行的状态改变都是不可追踪的；
+    Actions可以包含任何的异步操作
     
 */
+import { mapActions } from 'vuex';
+methods: {
+    // 多次调用的话，这样很方便，但是如果只是一次的话，那就没必要了
+    ...mapActions([
+        'actions' // 将this.actions() 映射为this.$store.dispatch('actions');
+    ]) 
+}
+// 
+actions: {
+    async actionA ({ commit }) {
+        commit('GETDATA', await getData());
+    },
+    async actionB ({commit, dispatch}) {
+        await dispatch('actionA');
+        commit('GETOTHERDATA', await getOtherData());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
