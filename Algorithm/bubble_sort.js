8f62e498cdaa11c9162d5c6601f1337cf1201cab
zeroone001 <ilylys@163.com>
2019-04-03 16:28:52 +0800

    插入排序

--- Algorithm/bubble_sort.js
+++ Algorithm/bubble_sort.js
@@ -2,8 +2,10 @@
     冒泡排序
 */
 const generateRandomArr = require('./generate-random.js');
+let _arr = generateRandomArr(10, 5, 100);
+
 function buddleSort (arr) {
     // body...
-    
+
 }
 
--- Algorithm/insert_sort.js
+++ Algorithm/insert_sort.js
@@ -1,10 +1,24 @@
 /*
     插入排序
+    将元素插入已经排好序的前面的数组中
+    相当于有两个数组，每次都跟前面的有序的数组的最后一个开始比较
 */
 const generateRandomArr = require('./generate-random.js');
 
 let _arr = generateRandomArr(10, 5, 100);
 
 function InsertSort (arr) {
-     
+    const len = arr.length;
+     for (let i = 1; i < len; i++) {
+        for (let j = i; j > 0; j--) {
+            if (arr[j] < arr[j-1]) {
+                [arr[j], arr[j-1]] = [arr[j-1], arr[j]];
+            } else {
+                break;
+            }
+        }
+     }
+     return arr;
 }
+
+console.log(InsertSort(_arr));
