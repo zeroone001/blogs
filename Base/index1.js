978ed960594412b7357cf9af462720e4751351a3
zeroone001 <ilylys@163.com>
2019-04-08 10:09:06 +0800

    update

--- Base/index1.js
+++ Base/index1.js
@@ -6,8 +6,52 @@
 */
 /*
     对象转原始类型
-    
+    Symbol.toPrimitive 的优先级最高
 */
+var a = {
+    i: 1,
+    valueOf() {
+        return a.i++;
+    },
+    toString() {
+        return a.i++;
+    },
+    [Symbol.toPrimitive]() {
+        return a.i++;
+    }
+}
+
+if ( a == 1 && a == 2 && a == 3 ) {
+  console.log(1);
+}
+/*
+    四则运算符
+*/
+true + true // 2
+
+'a' + + 'b' // 'aNaN'
++'b' === NaN
+// 除了加法之外，其他的运算符，只要一方是数字，另一方也会转换为数字
+4 * '3' === 12
+4 * [1, 2] === NaN
+4 * [] === 0
+// 比较运算符
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
