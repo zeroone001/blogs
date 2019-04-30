f8cc228fd441f056acd6f97927ab46e13087e8a0
zeroone001 <ilylys@163.com>
2019-04-30 14:58:05 +0800

    update

--- JavaScript/Interview/call_apply.js
+++ JavaScript/Interview/call_apply.js
@@ -31,12 +31,60 @@ Function.prototype.apply = function (context, arr) {
     return result;
 }
 /*
-    手写一个promise
+    手写一个promise 简单版本
 */
-function Promise () {
-    
+const PENDING = 'pending';
+const RESOLVED = 'resolved';
+const REJECTED = 'rejected';
+
+function Promise (fn) {
+    const that = this;
+    this.value = null;
+    this.state = PENDING;
+    that.resolvedCallbacks = [];
+    that.rejectedCallbacks = [];
+
+    function resolve (value) {
+        if (that.state === PENDING) {
+            that.state = RESOLVED;
+            that.value = value;
+            that.resolvedCallbacks.map(cb => cb(that.value));
+        }
+    }
+
+    function reject (value) {
+        if (that.state === PENDING) {
+            that.state = REJECTED;
+            that.value = value;
+            that.rejectedCallbacks.map(cb => cb(that.value));
+        }
+    }
+
+    try {
+        fn(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
 }
 
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    const that = this;
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v;
+    onRejected = typeof onRejected === 'function' ? onRejected : v => v;
+
+    if (that.state === PENDING) {
+        that.resolvedCallbacks.push(onFulfilled);
+        that.rejectedCallbacks.push(onRejected);
+    }
+
+    if (that.state === RESOLVED) {
+        onFulfilled(that.value);
+    }
+
+    if (that.state === REJECTED) {
+        onRejected(that.value);
+    }
+}
 
 
 
