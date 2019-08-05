71e0042634c014731b75e78a7cd716936edd90b5
zeroone001 <ilylys@163.com>
2019-08-05 13:40:55 +0800

    update

--- ADaily/Aug1.js
+++ ADaily/Aug1.js
@@ -24,5 +24,20 @@ function blur () {
     window.scroll(0, 0);
 }
 
+// 自定义输入框光标的颜色
+caret-color: auto;
+
+// https://blog.csdn.net/qq_42354773/article/details/87975287
+var oHeight = $(document).height();
+$(window).resize(function () {
+    if ($(document).height() < oHeight) {
+        $('.btn-box').addClass('btn-absolute');
+        //当软键盘弹出，在这里面操作
+    } else {
+        //当软键盘收起，在此处操作
+        $('.btn-box').removeClass('btn-absolute');
+    }
+});
+
 
 
