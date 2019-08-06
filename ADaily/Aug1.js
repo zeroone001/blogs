808a630fceabcdeb7d8d66cd6e79cbed1814a71d
zeroone001 <ilylys@163.com>
2019-08-06 14:02:32 +0800

    update

--- ADaily/Aug1.js
+++ ADaily/Aug1.js
@@ -39,5 +39,57 @@ $(window).resize(function () {
     }
 });
 
+function handleEvent () {
+    console.log('handleEvent');
+    $('.el-input__inner').each(function(index, item) {
+        if (index === 0) {
+            $(item).attr('placeholder', '请输入品牌的中文/英文名称');
+        } else if (index === 1) {
+            $(item).attr('placeholder', '参考源/官网/电商链接');
+        }
+        if (!isAndroid) {
+            $(item).focus(function() {
+                isFocus = true;
+                $('.btn-box').addClass('btn-absolute');
+            }).blur(function() {
+                isFocus = false;
+                setTimeout(function(){
+                    if (!isFocus) {
+                        window.scroll(0, 0);
+                    }
+                }, 500);
+                $('.btn-box').removeClass('btn-absolute');
+            });
+        }
+    });
+    $('.el-textarea__inner').attr('placeholder', '可填写品牌的定位，主营产品，使用感受等，最多200字哦~');
+    if (!isAndroid) {
+        $('.el-textarea__inner').focus(function() {
+            isFocus = true;
+            $('.btn-box').addClass('btn-absolute');
+        }).blur(function() {
+            isFocus = false;
+            setTimeout(function(){
+                if (!isFocus) {
+                    window.scroll(0, 0);
+                }
+            }, 500);
+            $('.btn-box').removeClass('btn-absolute');
+        });
+    }
+    if (isAndroid) {
+        var oHeight = $(document).height();
+        $(window).resize(function () {
+            if ($(document).height() < oHeight) {
+                $('.btn-box').addClass('btn-absolute');
+                //当软键盘弹出，在这里面操作
+            } else {
+                //当软键盘收起，在此处操作
+                $('.btn-box').removeClass('btn-absolute');
+            }
+        });
+    }
+}
+
 
 
