3893083c90c46602f4aa6d1d1217e05b7b2faea6
zeroone001 <ilylys@163.com>
2019-08-15 10:45:22 +0800

    update

---
+++ ADaily/Aug2.js
@@ -0,0 +1,31 @@
+// 下面两个函数可以用来获取绝对位置的横坐标和纵坐标。
+
+　function getElementLeft(element){
+　　　　var actualLeft = element.offsetLeft;
+　　　　var current = element.offsetParent;
+
+　　　　while (current !== null) {
+　　　　　　actualLeft += current.offsetLeft;
+　　　　　　current = current.offsetParent;
+　　　　}
+
+　　　　return actualLeft;
+　　}
+
+　　function getElementTop(element){
+　　　　var actualTop = element.offsetTop;
+　　　　var current = element.offsetParent;
+
+　　　　while (current !== null){
+　　　　　　actualTop += current.offsetTop;
+　　　　　　current = current.offsetParent;
+　　　　}
+
+　　　　return actualTop;
+　　}
+
+// 第二种简单方法
+
+var x = this.getBoundingClientRect().top + document.documentElement.scrollTop;
+
+
