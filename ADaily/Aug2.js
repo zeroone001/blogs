37351e120802639abff6989e7e702e3e671c34db
zeroone001 <ilylys@163.com>
2019-08-16 14:26:49 +0800

    update

--- ADaily/Aug2.js
+++ ADaily/Aug2.js
@@ -1,5 +1,5 @@
 // 下面两个函数可以用来获取绝对位置的横坐标和纵坐标。
-
+// http://www.ruanyifeng.com/blog/2009/09/find_element_s_position_using_javascript.html
 　function getElementLeft(element){
 　　　　var actualLeft = element.offsetLeft;
 　　　　var current = element.offsetParent;
