5f33c8afd7f54269fad1e8ef67b292ce944659ae
zeroone001 <ilylys@163.com>
2019-08-19 11:57:03 +0800

    update

---
+++ Flutter/setUp.js
@@ -0,0 +1,28 @@
+/*
+    1. download flutter
+    2. 放置flutter解压缩之后的文件夹
+    3. cd flutter && pwd -> 获取到[路径]
+    4. .zshrc 文件 && export PATH="$PATH:/路径/flutter/bin"
+    5. sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+    6. sudo xcodebuild -license
+    7. 打开IOS模拟器 && open -a Simulator
+    
+*/
+/*
+    create APP
+
+    1. To hot reload changes while running, press "r". To hot restart (and rebuild
+state), press "R".
+
+    2. An Observatory debugger and profiler on iPhone X is available at:
+http://127.0.0.1:56484/OiXrkQmphmk=/
+    3. For a more detailed help message, press "h". To detach, press "d"; to quit,
+press "q".
+ sudo mkdir -p /usr/local/sbin
+  sudo chown -R $(whoami) /usr/local/sbin
+*/
+
+/*
+    On macOS Mojave, the chown command needs to be run on the specific directory, an example for the permissions issue in /usr/local/lib directory, this command would be
+    sudo chown -R "$(whoami)":admin /usr/local/lib
+*/
