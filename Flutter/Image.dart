7b1ff3146f1d5795598e764472de6aa92b05b2d1
zeroone001 <ilylys@163.com>
2019-08-23 11:31:45 +0800

    update

--- Flutter/Image.dart
+++ Flutter/Image.dart
@@ -1,3 +1,12 @@
-Image(
-image: AssetImage("")
-    )
+
+void Test () {
+  Image(
+    image: AssetImage("images/avatar.png"),
+    width: 100.0
+  )
+
+  Image.asset("images/avatar.png", width: 100.0);
+
+  Image.network("https://avatars2.githubusercontent.com/u/20411648?s=460&v=4", width: 100.0);
+
+}
