0b34cb55302bfe687406c3091be6afa667c7ab42
zeroone001 <ilylys@163.com>
2019-09-23 18:56:07 +0800

    修改reader

--- Mobile/IOS_h5.js
+++ Mobile/IOS_h5.js
@@ -2,3 +2,9 @@
     <input type="number" pattern="[0-9]*"/>
     掉起文字键盘
 */
+var reader = new FileReader();
+reader.readAsDataURL(this.file[i]);
+reader.onload = function () {
+    console.log(this.result);
+    // this.result 是base64 的格式
+};
